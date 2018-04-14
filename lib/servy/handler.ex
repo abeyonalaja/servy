372efe8b2a653ef6a714f94a5ad6ed4150402509
Abideen Onalaja <abideenonalaja@Abideens-MacBook-Pro.local>
2018-04-14 22:06:45 +0100

    request params

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -4,10 +4,13 @@ defmodule Servy.Handler do
 
     request
     |> parse
+    |> log
     |> route
     |> format_response
 
   end
+
+  def log(conv), do: IO.inspect conv
 
   def parse(request) do
     [ method, path, _ ] =
@@ -16,24 +19,55 @@ defmodule Servy.Handler do
       |> List.first
       |> String.split(" ")
 
-    conv = %{ method: method, path: path, resp_body: "" }
+    %{ method: method,
+       path: path,
+       resp_body: "",
+       status: nil
+    }
   end
 
   def route(conv) do
-    conv = %{ conv | resp_body: "Bears, Lions, Tigers" }
+    route(conv, conv.method, conv.path)
+  end
+
+  def route(conv, "GET", "/wildthings") do
+    %{ conv | status: 200, resp_body: "Bears, Lions, Tigers" }
+  end
+
+  def route(conv, "GET", "/bears") do
+    %{ conv | status: 200, resp_body: "Teaddy, Smokey, Paddington" }
+  end
+
+  def route(conv, "GET", "/bears/" <> id) do
+    %{ conv | status: 200, resp_body: "Bear #{id}" }
+  end
+
+  def route(conv, _method, path) do
+    %{ conv | status: 404, resp_body: "No #{path} here!" }
   end
 
   def format_response(conv) do
      """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{conv.status} #{status_response( conv.status )}
     Content-Type: text/html
     Content-Length: #{ String.length( conv.resp_body ) }
 
     #{ conv.resp_body }
     """
   end
-end
 
+  defp status_response(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
+  end
+
+end
 request = """
 GET /wildthings HTTP/1.1
 Host: example.com
@@ -41,6 +75,18 @@ User-Agent: ExampleBrowser/1.0
 Accept: */*
 
 """
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /bears/2 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+    """
 
 response = Servy.Handler.handle(request)
 
